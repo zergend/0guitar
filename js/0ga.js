7965c828377d340ab45f03eaa44ada6d3c934b71
zergend <zergend@gmail.com>
2022-03-01 23:30:18 +0300

    feat: add normalize.css, visual duration of loop

--- js/0ga.js
+++ js/0ga.js
@@ -1,17 +1,27 @@
 // import Plyr as Plyr from './plyr.js';
 
 // const player = new Plyr('#player');
+let startTime = 254;
+let endTime = 264;
 
 const player = new Plyr(document.getElementById('player'));
 
+
 const btn = document.querySelector('.btn__video');
+
 const check = document.querySelector('.check__loop');
 
-btn.textContent = 'Как играется';
+function goLoad() {
+    btn.textContent = 'Как играется';
+    btn.style.marginLeft = (startTime / player.duration) * 100 + '%';
+    btn.style.width = ((endTime - startTime) / player.duration) * 100 + '%';
+}
+
+window.addEventListener('load', goLoad);
+
 
 function goPlay() {
-
-    player.currentTime = 254; // теперь о том как она играется (спокойная ночь)
+    player.currentTime = startTime; // теперь о том как она играется (спокойная ночь)
     player.muted = false;
 
     player.play(); // Start playback
@@ -22,12 +32,12 @@ btn.addEventListener('click', goPlay);
 player.on('timeupdate', (event) => {
 
     if (check.checked) {
-        if (player.currentTime >= 264) {
-            player.currentTime = 254;
+        if (player.currentTime >= endTime) {
+            player.currentTime = startTime;
 
             player.play();
         }
     } else {
-        if (player.currentTime >= 264) player.pause();
+        if (player.currentTime >= endTime) player.pause();
     }
 });
